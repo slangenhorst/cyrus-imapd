a88c7547c7b1c265062dec2524bdb94f16f670e8
Bron Gondwana <brong@fastmail.fm>
2022-03-16 10:59:18 -0400

    Cassandane: convert from Blob/set to Blob/upload

--- cassandane/Cassandane/Cyrus/JMAPEmail.pm
+++ cassandane/Cassandane/Cyrus/JMAPEmail.pm
@@ -19635,7 +19635,7 @@ EOF
 
     xlog $self, "do the lot!";
     $res = $jmap->CallMethods([
-            ['Blob/set', { create => { "a" => { 'data:asText' => $email } } }, 'R0'],
+            ['Blob/upload', { create => { "a" => { data => [{'data:asText' => $email }] } } }, 'R0'],
             ['Email/import', {
             emails => {
                 "1" => {
@@ -19659,7 +19659,7 @@ EOF
     close(FH);
 
     $res = $jmap->CallMethods([
-            ['Blob/set', { create => { "img" => { 'data:asBase64' => encode_base64($binary, ''), type => 'image/gif' } } }, 'R0'],
+            ['Blob/upload', { create => { "img" => { data => [{'data:asBase64' => encode_base64($binary, '')}], type => 'image/gif' } } }, 'R0'],
             ['Email/set', {
             create => {
                 "2" => {
